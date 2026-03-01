ff07cd915801d254989ba4f94b02294280a754d4
realtimml <timmyphan066@gmail.com>
2026-02-28 19:37:30 -0800

    Change frame rate to 15 fps

--- python/check_in_frame.py
+++ python/check_in_frame.py
@@ -6,8 +6,6 @@ model = YOLO('python/yolo26n-pose.pt')  # Load a pretrained YOLOv8 model (you ca
 
 # 1. Initialize the webcam (0 is usually the default built-in camera)
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 854)  # Set width
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)  # Set height
 
 if not cap.isOpened():
     print("Error: Could not open webcam.")
@@ -28,7 +26,7 @@ def check_in_frame(capture, model) -> Generator[bool, None, None]:
         # annotated_frame = results[0].plot()
         # cv2.imshow('Webcam Feed', annotated_frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(66) & 0xFF == ord('q'):
             break
 
         yield len(results[0].boxes) > 0  # Yield True if at least one person is detected, otherwise False
