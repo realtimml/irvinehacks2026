588c63bb43f629761ba872fb0fb8b572a4b8c5a1
realtimml <timmyphan066@gmail.com>
2026-02-28 19:43:25 -0800

    Decrease to 288p

--- python/check_in_frame.py
+++ python/check_in_frame.py
@@ -7,6 +7,7 @@ model = YOLO('python/yolo26n-pose.pt')  # Load a pretrained YOLOv8 model (you ca
 # 1. Initialize the webcam (0 is usually the default built-in camera)
 cap = cv2.VideoCapture(0)
 
+
 if not cap.isOpened():
     print("Error: Could not open webcam.")
     exit()
@@ -17,16 +18,17 @@ print("Press 'q' to quit.")
 def check_in_frame(capture, model) -> Generator[bool, None, None]:
     while True:
         ret, frame = capture.read()
+        frame = cv2.resize(frame, (512, 288))  # Resize frame for faster processing
 
         if not ret:
             print("Error: Can't receive frame.")
             return False
 
         results = model(frame)
-        # annotated_frame = results[0].plot()
-        # cv2.imshow('Webcam Feed', annotated_frame)
+        annotated_frame = results[0].plot()
+        cv2.imshow('Webcam Feed', annotated_frame)
 
-        if cv2.waitKey(66) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
         yield len(results[0].boxes) > 0  # Yield True if at least one person is detected, otherwise False
